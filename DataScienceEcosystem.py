15995107b7ef60a5d4d5fe377617657fd0f22231
Chetna-Bendale <79893890+Chetna-Bendale@users.noreply.github.com>
2023-07-20 01:16:45 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ###### Some of the popular languages that Data Scientists use are:
+#     1. Python
+#     2. R
+#     3. SQL
+#     4. Scala
+#     5. Java
+#     6. C++
+#     7. Julia
+#     8 JavaScript
+#     9. PHP
+#     10. Go
+#     11. Ruby
+#     12. Visual Basic
+
+# ###### Some of the popular libraries that Data Scientists use are:
+#     1. Pandas
+#     2. Numpy
+#     3. Matplotlib
+#     4. Seaborn
+#     5. Scikit-Learn
+#     6. TensorFlow
+#     7. PyTorch
+#     8 Apache Spark
+#     9. Vegas
+#     10. Big DL
+#     11. Ggplot2
+#     12. Keras
+
+# | Data Science Tools |
+# | ------------------- |
+# | Anaconda |
+# | Jupyter Notebooks |
+# | R Studio |
+# | Spyder |
+# | Zeppelin |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[7]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200//60
+
+
+# **Objectives:**
+# + Create markdown cells
+# + Add ordered and unordered lists to markdown cells
+# + List popular languages for Data Science
+
+# ## Author(s)
+# Chetna
+
+# In[ ]:
+
+
+
+
